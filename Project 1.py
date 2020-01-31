9a978e6f780ecb3fecf7e4b49a7d6e2f8e881e3b
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2020-01-31 23:00:03 +0200

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,25 @@
+# This is a dynamic Grade calculator
+# Can calculate any count of subjects in the following form
+# Name - Subject1 - Subject2 - ... - SubjectN - TOTAL - PERCENT
+import openpyxl as xl
+
+# Load workbook and default sheet "Sheet1"
+wb = xl.load_workbook('database.xlsx')
+sheet = wb['Sheet1']
+
+# CONSTANTS
+## last column non-zero based index
+MAX_C = sheet.max_column
+## global total degree
+FULL_DEGREE = (MAX_C - 3) * 100
+
+for row in range(2, sheet.max_row + 1):
+    # Calculate Total degree
+    total = sum(sheet.cell(row, _cell).value for _cell in range(2, sheet.max_column - 1))
+    
+    # We calculate total and percent
+    sheet.cell(row, MAX_C - 1).value = total
+    sheet.cell(row, MAX_C).value = total * 100 / FULL_DEGREE
+
+# Save as new workbook
+wb.save('database_processed.xlsx')
