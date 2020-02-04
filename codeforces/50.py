ec9fa8b116da8c69b30337222410b2c9ac661327
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2020-02-04 12:58:25 +0200

    Update 50.py

--- codeforces/50.py
+++ codeforces/50.py
@@ -1,3 +1,3 @@
 # https://codeforces.com/problemset/submission/50/70213839
 
-print(int((lambda x, y: x*y)(*map(int, input().split())) / 2))
+print(int((lambda x, y: x * y)(*map(int, input().split())) / 2))
